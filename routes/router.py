f77dc05b84be78b07ad8747a7eecb99df4f3add9
Rodrigo Anjinho Gomes <rodrigo.anjinho.prestserv@transpetro.com.br>
2025-06-02 11:43:28 -0300

    feat(Criação-de-rotas): Rotas para operações de usuários
    Rotas HTTP para Criação(POST), Listagem(GET), Remoção(DELETE) e atualização(PUT/PATCH)
    
    SDIT-0001/GW-000001

---
+++ routes/router.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, Path
+from schemas import UserList, UserItem, UserOperations
+from pydantic import TypeAdapter
+
+router = APIRouter()
+
+@router.get('/user', response_model=UserList)
+async def list_user():
+	"""
+	Operação de Listagem de todos os usuários da base.
+	"""
+	user = UserItem(username='Rodrigo ', first_name='Rodrigo', last_name='Gomes')
+	response = UserList(status_code=200, message='Lista de usuários', items=[user])
+	return TypeAdapter(UserList).dump_python(
+		response, by_alias=True
+	)
+
+@router.post('/user')
+async def create_user():
+	
+	return {
+		'status_code': 200,
+		'message': 'Usuário Criado com sucesso'
+	}
+
+@router.patch('/user/{id}', response_model=UserOperations)
+async def update_partial_user(id: int = Path()):
+	return {
+		'status_code': 200,
+		'message': 'Usuário Atualizado com sucesso'
+	}
+
+@router.put('/user/{id}', response_model=UserOperations)
+async def update_full_user(id: int = Path()):
+	return {
+		'status_code': 200,
+		'message': 'Usuário Atualizado com sucesso'
+	}
+
+@router.delete('/user/{id}', response_model=UserOperations)
+async def delete_user(id: int = Path()):
+	return {
+		'status_code': 200,
+		'message': 'Usuário Deletado com sucesso'
+	}
