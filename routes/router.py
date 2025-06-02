70b240ce8ed3d6a01aa8b4b9db94ae99cefc4df8
Rodrigo Anjinho Gomes <rodrigo.anjinho.prestserv@transpetro.com.br>
2025-06-02 11:53:10 -0300

    docs: Api name e version, documentação de rotas
    Adição do nome e versão da api, inclusão de comentários de documentação de rotas
    
    SDIT-0001/GW-000001

--- routes/router.py
+++ routes/router.py
@@ -4,42 +4,44 @@ from pydantic import TypeAdapter
 
 router = APIRouter()
 
-@router.get('/user', response_model=UserList)
+
+@router.get("/user", response_model=UserList)
 async def list_user():
-	"""
-	Operação de Listagem de todos os usuários da base.
-	"""
-	user = UserItem(username='Rodrigo ', first_name='Rodrigo', last_name='Gomes')
-	response = UserList(status_code=200, message='Lista de usuários', items=[user])
-	return TypeAdapter(UserList).dump_python(
-		response, by_alias=True
-	)
+    """
+    Operação de Listagem de todos os usuários da base.
+    """
+    user = UserItem(username="Rodrigo ", first_name="Rodrigo", last_name="Gomes")
+    response = UserList(status_code=200, message="Lista de usuários", items=[user])
+    return TypeAdapter(UserList).dump_python(response, by_alias=True)
 
-@router.post('/user')
+
+@router.post("/user")
 async def create_user():
-	
-	return {
-		'status_code': 200,
-		'message': 'Usuário Criado com sucesso'
-	}
+    """
+    Operação de criação de usuários na base.
+    """
+    return {"status_code": 200, "message": "Usuário Criado com sucesso"}
 
-@router.patch('/user/{id}', response_model=UserOperations)
+
+@router.patch("/user/{id}", response_model=UserOperations)
 async def update_partial_user(id: int = Path()):
-	return {
-		'status_code': 200,
-		'message': 'Usuário Atualizado com sucesso'
-	}
+    """
+    Operação de update parcial de usuários na base.
+    """
+    return {"status_code": 200, "message": "Usuário Atualizado com sucesso"}
 
-@router.put('/user/{id}', response_model=UserOperations)
+
+@router.put("/user/{id}", response_model=UserOperations)
 async def update_full_user(id: int = Path()):
-	return {
-		'status_code': 200,
-		'message': 'Usuário Atualizado com sucesso'
-	}
+    """
+    Operação de update total de usuários na base.
+    """
+    return {"status_code": 200, "message": "Usuário Atualizado com sucesso"}
 
-@router.delete('/user/{id}', response_model=UserOperations)
+
+@router.delete("/user/{id}", response_model=UserOperations)
 async def delete_user(id: int = Path()):
-	return {
-		'status_code': 200,
-		'message': 'Usuário Deletado com sucesso'
-	}
+    """
+    Operação de delete de usuários na base.
+    """
+    return {"status_code": 200, "message": "Usuário Deletado com sucesso"}
--- webserver.py
+++ webserver.py
@@ -2,5 +2,5 @@ from fastapi import FastAPI
 from routes.router import router
 
 
-app = FastAPI()
+app = FastAPI(title='Gestão de Código', version='0.0.2')
 app.include_router(router)
